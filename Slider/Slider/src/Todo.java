06408a005ec2c62e067a45f5094fc74ba37d7ffa
Bill Wake <william.wake@acm.org>
2011-01-18 21:30:42 -0500

    Delete unused files (at least I hope they're unused). WW

--- Slider/Slider/src/Todo.java
+++
@@ -1,6 +0,0 @@
-
-public class Todo {
-//* create a pool of candidates
-//* some algo to walk through pool and find best match (greedy first)
-//* Make sure AAA then BBB in same column shows up as ===--- not ======
-}
